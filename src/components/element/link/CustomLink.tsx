7f5a1d11793ac7ca4f0e6ee2175670eb4050f203
Yu Jin Choe <forworkchoe@gmail.com>
2024-07-22 22:19:30 +0900

    ActiveLink / CustomLink로 분리

--- src/components/element/link/CustomLink.tsx
+++ src/components/element/link/CustomLink.tsx
@@ -1,6 +1,16 @@
-export type CustomLinkProps = any;
-export type LinkActiveMode = any;
+import Link, {LinkProps} from 'next/link';
+import React, {CSSProperties, PropsWithChildren} from 'react';
 
-export default function CustomLink(_: any) {
-  return null;
+export interface CustomLinkProps extends PropsWithChildren<LinkProps> {
+  style?: CSSProperties;
+  className?: string;
+}
+
+/**
+ * Original : https://github.com/developer-choi/react-playground/commit/80891494f84e1c93c42fb01428d1dbee98133601#diff-c5022c9c2a2b3525d5b37b200fb8fea5c4ea7b1775ac34558c227a5267fdd99f
+ */
+export default function CustomLink({prefetch = false, ...rest}: CustomLinkProps) {
+  return (
+    <Link prefetch={prefetch} {...rest}/>
+  );
 }

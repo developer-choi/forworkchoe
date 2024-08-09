16af9df85aedec8820e8948ce4a1dc6f6b99ed38
Yu Jin Choe <forworkchoe@gmail.com>
2024-08-09 10:48:08 +0900

    푸터 컨테이너 별도분리

--- src/components/modal/footer.tsx
+++ src/components/modal/footer.tsx
@@ -4,7 +4,7 @@ import {ComponentPropsWithoutRef} from 'react';
 import Button, {ButtonProps} from '@/components/element/Button';
 
 // 일단 footer 태그의 전체 props를 확장함으로써 혹시모를 커스텀 대비
-export interface OneButtonModalFooterProps extends Omit<ComponentPropsWithoutRef<'footer'>, 'children'> {
+export interface OneButtonModalFooterProps extends Omit<ModalFooterProps, 'children'> {
   buttonProps?: Pick<ButtonProps, 'children' | 'onClick' | 'type' | 'className' | 'style'>;
 }
 
@@ -14,8 +14,17 @@ export interface OneButtonModalFooterProps extends Omit<ComponentPropsWithoutRef
  */
 export function OneButtonModalFooter({className, buttonProps, ...rest}: OneButtonModalFooterProps) {
   return (
-    <footer className={classNames(styles.oneButtonFooter, className)} {...rest}>
+    <ModalFooter className={classNames(styles.oneButtonFooter, className)} {...rest}>
       <Button {...buttonProps}/>
-    </footer>
+    </ModalFooter>
   );
+}
+
+type ModalFooterProps = ComponentPropsWithoutRef<'footer'>;
+
+// 추후 어떤 푸터가 추가 되더라도, 푸터 껍데기 (하단간격, 좌우간격)는 스타일이 변하지않음.
+function ModalFooter({className, ...rest}: ModalFooterProps) {
+  return (
+    <footer className={classNames(styles.oneButtonFooter, className)} {...rest}/>
+  )
 }

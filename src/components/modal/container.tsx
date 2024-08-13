123713165f86e866f56d3f1b329fd39b7ad045a8
Yu Jin Choe <forworkchoe@gmail.com>
2024-08-13 20:47:53 +0900

    모달 주석추가

--- src/components/modal/container.tsx
+++ src/components/modal/container.tsx
@@ -9,15 +9,30 @@ import {EssentialModalProps} from '@/util/extend/modal';
 export interface ModalContainerProps extends EssentialModalProps, Omit<ComponentPropsWithoutRef<'div'>, 'onClick'> {
   easilyClose?: boolean; // backdrop / esc 눌러서 모달 닫게 해주는 기능, default false
   type: 'centerAlign' | 'bottomSheet' | 'fullScreen';
-  size?: 'large' | 'medium'; // center-align 모달에서만 사용함
+  size?: 'large' | 'medium'; // centerAlign 모달에서만 사용함
+
+  /**
+   * bottomSheet 모달에서는 boolean 으로 전달, 나머지는 전달할 필요는 없지만 만약 전달한 경우, 값에 따라 동작됨.
+   * bottomSheet 모달은 Store 모달이 아닌 State 모달로 사용하기.
+   * 나머지 모달은 Store / State모달 둘 다 의도대로 동작함.
+   *
+   * 이 모든것의 근본적인 이유는,
+   * 1. bottomSheet는 애니메이션 때문에 항상 렌더링되어있어야 하고 (끄더라도...)
+   * 2. 나머지는 뜰 때 마운트, 닫힐 때 언마운트 되도 상관없다는 차이로 인해 발생함.
+   */
   open?: boolean;
 }
 
 // Alert, Confirm 등 모달 확장할 때 이 타입을 확장하기
 export type ComposedModalProps = Omit<ModalContainerProps, 'type' | 'size'>;
 
-export function ModalContainer({className, size = 'medium', type, easilyClose, onClose, open = true, ...rest}: ModalContainerProps) {
+export function ModalContainer({className, size = 'medium', type, easilyClose, onClose, open, ...rest}: ModalContainerProps) {
   const modalRef = useRef<HTMLDialogElement>(null);
+  /**
+   * bottomSheet면 기본값 false로 동작하고
+   * 나머지는 기본값 true로 동작함.
+   */
+  const _open = type === 'bottomSheet' ? !!open : open ?? true;
 
   useEffect(() => {
     const escHandler = (event: KeyboardEvent) => {
@@ -44,17 +59,17 @@ export function ModalContainer({className, size = 'medium', type, easilyClose, o
       modal?.close();
       document.body.style.overflow = 'auto';
     };
-    
-    if (open === false) {
+
+    if (!_open) {
       closeModal();
       return;
     }
-    
+
     modal?.showModal();
     document.body.style.overflow = 'hidden';
 
     return closeModal;
-  }, [open]);
+  }, [_open]);
 
   const onClickBackdrop = useCallback((event: MouseEvent<HTMLDivElement>) => {
     if (type === 'fullScreen') {
@@ -66,7 +81,7 @@ export function ModalContainer({className, size = 'medium', type, easilyClose, o
 
   // https://docs.google.com/document/d/16-Z3RmslEMvhfwOMmePYTRg4HkUjKjWSxxF2SB7NGGU/edit#heading=h.dpm2y2x8qp1i
   return createPortal((
-    <dialog ref={modalRef} className={classNames(styles[type + 'Container'], {[styles.open]: open})} onClick={easilyClose ? onClose : undefined}>
+    <dialog ref={modalRef} className={classNames(styles[type + 'Container'], {[styles.open]: _open})} onClick={easilyClose ? onClose : undefined}>
       <div className={classNames(styles.rootModalInner, styles[type + 'Inner'], styles[size], className)} onClick={onClickBackdrop} {...rest} />
     </dialog>
   ), document.body);

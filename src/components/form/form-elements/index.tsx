4fc687dafc5644ed2a43a1b11b7c4724c4244e95
Yu Jin Choe <forworkchoe@gmail.com>
2024-08-16 19:58:46 +0900

    Revert "Input, TextArea, RadioGroup 컨테이너 스타일 조정 및 태그 종류 조정"
    This reverts commit 266051fb0785654a083654d114fd5cb9e5800f81.

--- src/components/form/form-elements/index.tsx
+++ src/components/form/form-elements/index.tsx
@@ -19,18 +19,13 @@ export interface FormElementWrapperProps {
  * 네이버기준 disabled > error > focus > default임.
  */
 export function FormElementWrapper({label, error, children, style, className = ''}: PropsWithChildren<FormElementWrapperProps>) {
-  /**
-   * 전체 폼요소 영역을 label로 감싸서, 라벨 / 에러메시지 / 테두리와 실제 폼요소 사이 간격등 어디를 클릭하더라도 폼요소로 포커스 이동하도록 하기위함.
-   * <Input> 에서 단위로 points 같은거 노출된 상태에서, points 텍스트 영역을 클릭했을 때 인풋 폼요소로 포커스이동이 안되서 추가하긴 했지만, (일단 테두리 안에있긴하니까)
-   * 그냥 이거로 감싸지는 모든 폼요소에 적용해도 문제는 없어보여서 이렇게 처리했음.
-   */
   return (
-    <label style={style} className={classNames(styles.container, className)}>
-      {!label ? null : <span className={styles.label}>{label}</span>}
+    <div style={style} className={classNames(className)}>
+      {!label ? null : <label className={styles.label}>{label}</label>}
 
       {children}
 
       {!error ? null : <span className={styles.formErrorMessage}>{error}</span>}
-    </label>
+    </div>
   )
 }

77d8af886a61f750734fe11ded0917bc756c5c6c
Yu Jin Choe <forworkchoe@gmail.com>
2024-08-16 19:58:54 +0900

    Revert "Loading Spinner stroke ==> fill로 네이밍 변경"
    This reverts commit ec2f93a9cda4d08fe17735a4a31bd2019ba35094.

--- src/components/element/LoadingSpinner/index.tsx
+++ src/components/element/LoadingSpinner/index.tsx
@@ -2,16 +2,17 @@ import {ComponentPropsWithRef} from "react";
 import styles from './index.module.scss';
 import classNames from 'classnames';
 
+// 기본 크기는 50x50에 실제 스피너 돌아가는 부분은 20x20임.
 interface SpinnerProps extends ComponentPropsWithRef<"svg"> {
   strokeWidth?: number; // default 5
-  fill?: string;
+  stroke?: string;
   square?: number;
 }
 
-export default function LoadingSpinner({square = 20, strokeWidth = 3, fill = 'black', className, ...rest}: SpinnerProps) {
+export default function LoadingSpinner({square = 20, strokeWidth = 3, stroke = 'black', className, ...rest}: SpinnerProps) {
   return (
     <svg viewBox={`0 0 ${square} ${square}`} width={square} height={square} className={classNames(className, styles.svg)} {...rest}>
-      <circle cx={square / 2} cy={square / 2} r={(square / 2) - (strokeWidth / 2)} fill="none" stroke={fill} strokeWidth={strokeWidth}></circle>
+      <circle cx={square / 2} cy={square / 2} r={(square / 2) - (strokeWidth / 2)} fill="none" stroke={stroke} strokeWidth={strokeWidth}></circle>
     </svg>
   );
 }

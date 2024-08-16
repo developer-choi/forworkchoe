3af65bafd0b4c8e2c82c4209b4fe200194d52d1f
Yu Jin Choe <forworkchoe@gmail.com>
2024-08-16 19:58:31 +0900

    Revert "모달 닫을 때 애니메이션을 opacity가 아닌 background-color 기반으로 변경"
    This reverts commit 2781f801ab9a9894909737b42c131b09a753c022.

--- src/app/design-system/modal/page.tsx
+++ src/app/design-system/modal/page.tsx
@@ -29,7 +29,7 @@ export default function DesignSystemTest() {
 function ClassifyModalTest() {
   const { register, watch, handleSubmit } = useForm<ModalFormData>({
     defaultValues: {
-      type: 'bottomSheet',
+      type: 'centerAlign',
       title: 'short',
       content: 'short',
       size: 'medium',

f1555eece4cec3c61de3d9e325d8ff1c68048500
Yu Jin Choe <forworkchoe@gmail.com>
2025-08-20 19:23:26 +0900

    모든 코드 삭제 및 package.json을 태초의 모습으로 수정

--- src/util/style/theme.ts
+++
@@ -1,9 +0,0 @@
-import type {DefaultTheme} from 'styled-components';
-
-export const COLORS = {
-  neonBlue: 'rgb(97, 218, 251)',
-};
-
-export const theme: DefaultTheme = {
-  main: 'lightcoral'
-};

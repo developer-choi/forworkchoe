afe20b29580951eee723f7af1f4db2149436c0e2
Yu Jin Choe <forworkchoe@gmail.com>
2025-08-23 01:27:53 +0900

    모든 스토리 파일에도 전역 css를 추가하고, 설치하는 입장에서 core.css 하나만 import 해도 전역 css까지 같이 import 되도록 수정

--- core/src/index.ts
+++ core/src/index.ts
@@ -1,4 +1,7 @@
-export {default as Button} from './components/form/Button';
-export {add} from './utils/calculator';
-export * from './utils/format';
-export * from './utils/something';
+import '@/styles/reset.css';
+import '@/styles/global.css';
+
+export {default as Button} from '@/components/form/Button';
+export {add} from '@/utils/calculator';
+export * from '@/utils/format';
+export * from '@/utils/something';
